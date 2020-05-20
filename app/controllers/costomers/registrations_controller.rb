57535a17273f796ecfc38f5a84d076cf1fea3cbb
kentoda <kent19950811@gmail.com>
2020-05-20 12:18:41 +0000

    修正2

--- app/controllers/costomers/registrations_controller.rb
+++ app/controllers/costomers/registrations_controller.rb
@@ -12,11 +12,7 @@ class Costomers::RegistrationsController < Devise::RegistrationsController
   # def new
   #   super
   # end
-   protected
-
-    def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
-    end
+   
 
 
   # POST /resource
@@ -48,7 +44,11 @@ class Costomers::RegistrationsController < Devise::RegistrationsController
   #   super
   # end
 
-  # protected
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:family_name, :middle_name, :family_name_kana, :middle_name_kana])
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_up_params
@@ -61,9 +61,9 @@ class Costomers::RegistrationsController < Devise::RegistrationsController
   # end
 
   # The path used after sign up.
-  # def after_sign_up_path_for(resource)
-  #   super(resource)
-  # end
+  def after_sign_up_path_for(resource)
+    costomer_path(current_costomer)
+  end
 
   # The path used after sign up for inactive accounts.
   # def after_inactive_sign_up_path_for(resource)
