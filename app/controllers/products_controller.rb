712168b995e0f585c2e18197795e651f94e2ebd0
“sss-o2” <o.matsu.youser12@gmail.com>
2020-05-21 12:47:03 +0000

    karacommit

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,8 +1,11 @@
 class ProductsController < ApplicationController
 	def index
+		@products = Product.page(params[:page]).per(8)
+		@genres =  Genre.where(is_valid: true)
 	end
 
 	def show
+
 	end
 	
 end
