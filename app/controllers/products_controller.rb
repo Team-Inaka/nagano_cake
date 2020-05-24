89e67ec0dcaccd6e0eab2d790e3cae2be18f5117
taichi-sato36 <energy.sato36@gmail.com>
2020-05-24 12:08:34 +0000

    test

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,6 +3,11 @@ class ProductsController < ApplicationController
 	end
 
 	def show
+		@product = Product.find(params[:id])
 	end
 	
+	private
+	def product_params
+		paarams.require(:product).permit(:name, :notax_price, :image_id. :genre_id, :is_valid)
+	end
 end
