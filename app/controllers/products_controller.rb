053f916a3ac786045e43058ded81fd147ab34d8d
taichi-sato36 <energy.sato36@gmail.com>
2020-05-24 12:40:34 +0000

    test

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,39 @@
+# class ProductsController < ApplicationController
+# 	def index
+# 		@genres =  Genre.where(is_valid: true)
+
+# 	    if params[:genre_id]
+# 	      # Genreのデータベースのテーブルから一致するidを取得
+# 	      @genre = Genre.find(params[:genre_id])
+# 	      # genre_idと紐づく商品を取得
+# 	      @products = @genre.products.all.page(params[:page]).per(8)
+# 	    else
+# 	      # 商品すべてを取得
+# 	      @products = Product.page(params[:page]).per(8)
+# 	    end
+# 	end
+
+# 	def show
+# 		@genres =  Genre.where(is_valid: true)
+# 		@product = Product.find(params[:id])
+
+# 		if @product.is_valid == true
+# 			@select_text=[1,2,3,4,5,6,7,8,9,10]
+# 			@b_name="カートに追加する"
+# 		else
+# 			select_text="個数選択"
+# 			@b_name="売り切れ"
+# 		end
+# 		@producted=Product.new# テスト用変数 後で消す
+
+# 	end
+
+# 	private
+# 	def product_params
+# 		params.require(:product).permit(:name, :notax_price, :image_id, :genre_id, :is_valid)
+# 	end
+# end
+
 class ProductsController < ApplicationController
 	def index
 		@genres =  Genre.where(is_valid: true)
@@ -5,6 +41,7 @@ class ProductsController < ApplicationController
 	    if params[:genre_id]
 	      # Genreのデータベースのテーブルから一致するidを取得
 	      @genre = Genre.find(params[:genre_id])
+	       
 	      # genre_idと紐づく商品を取得
 	      @products = @genre.products.all.page(params[:page]).per(8)
 	    else
@@ -25,11 +62,7 @@ class ProductsController < ApplicationController
 			@b_name="売り切れ"
 		end
 		@producted=Product.new# テスト用変数 後で消す
-
 	end
-
-	private
-	def product_params
-		params.require(:product).permit(:name, :notax_price, :image_id, :genre_id, :is_valid)
-	end
+	
 end
+
