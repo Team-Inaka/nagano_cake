388d5134f9da9472b201073be2c35fea99683ab1
kentoda <kent19950811@gmail.com>
2020-05-24 12:20:52 +0000

    aaa

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+	# 税込金額計算メソッド
+	def addTax(price)
+		(price*1.1).round
+	end
+
+	# 金額など3桁区切りカンマメソッド（1,000とか）
+	def add_comma(price)
+    	price.to_s(:delimited)
+  	end
+
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -7,4 +7,5 @@ class Product < ApplicationRecord
 
 	belongs_to :genre
 	attachment :image
+
 end
