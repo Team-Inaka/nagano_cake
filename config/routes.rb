d71600cc3910e028e609fb3a952db35a88ba9116
kentoda <kent19950811@gmail.com>
2020-05-27 06:36:23 +0000

    resolve conflict

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,7 @@ Rails.application.routes.draw do
    }
    #  :controllers => {
    #    :registrations => 'costomers/registrations',
+   
    #    :sessions => 'costomers/sessions',
    #    :passwords => 'costomers/passwords'
    #  }
