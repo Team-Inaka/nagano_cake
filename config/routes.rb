2aa0a2f898c90b5c7aef5c3a5a29768c0344f4ea
hiroki3756 <torotyuka0121@gmail.com>
2020-05-28 14:11:22 +0000

    before omatu-header

--- config/routes.rb
+++ config/routes.rb
@@ -4,19 +4,19 @@ Rails.application.routes.draw do
   # resources :cart_items
   # resources :carts, except: [:index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  # post "orders/confirm" => "orders#confirm"
-  # get "orders/complete" => "orders#complete"
-  # resources :orders,only:[:new,:create,:index,:show]
+  post "orders/confirm" => "orders#confirm"
+  get "orders/complete" => "orders#complete"
+  resources :orders,only:[:new,:create,:index,:show]
   
-  # namespace :admins do
-  #   resources :ordered_products, only:[:index,:show]
-  #   patch "ordered_products/:id/order_update" => "ordered_products#order_update", as: 'order_update'
-  #   patch "ordered_products/:id/production_update" => "ordered_products#production_update"
-  # end
+  namespace :admins do
+    resources :ordered_products, only:[:index,:show]
+    patch "ordered_products/:id/order_update" => "ordered_products#order_update", as: 'order_update'
+    patch "ordered_products/:id/production_update" => "ordered_products#production_update"
+  end
 
-  # namespace :admins do
-  #   resources :orders, only:[:index, :show, :update]
-  # end
+  namespace :admins do
+    resources :orders, only:[:index, :show, :update]
+  end
   
   
   
